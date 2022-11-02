9041917677c5cf860d95541f97230ce1c99a21be
SyurenChetty <104988398+SyurenChetty@users.noreply.github.com>
2022-11-02 17:02:26 +0200

    Add files via upload

---
+++ CalculateMod/CalculateMod/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateMod
+{
+    public class Class1
+    {
+        public static int SelfStudies(int valOne,int valTwo, int valThree)
+        {
+            return ((valOne * 10) / valTwo) - valThree;
+        }
+
+        public static int remainingHours(int ValOne, int ValTwo)
+        {
+            return ValOne - ValTwo;
+        }
+    }
+}
